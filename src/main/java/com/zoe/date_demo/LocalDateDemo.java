b6f5e2035feb7aae6cbfc8d80ed87af7c00d0a97
wllrx <wllrx93@163.com>
2019-09-11 17:31:31 +0800

    时间测试修改

--- src/main/java/com/zoe/date_demo/LocalDateDemo.java
+++ src/main/java/com/zoe/date_demo/LocalDateDemo.java
@@ -3,6 +3,7 @@ package com.zoe.date_demo;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author zoe
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 public class LocalDateDemo {
     public static void main(String[] args) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(simpleDateFormat.format(new Date()));
         LocalDateTime time = LocalDateTime.now();
         System.out.println("localDateTime的值为: "+time);
         LocalDate localDate = LocalDate.now();
